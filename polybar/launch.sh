585c810973df5b906144e16724d20450a1f31b64
JRozen <thequetran@gmail.com>
2018-10-29 08:57:57 +0100

    update launch sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,4 +9,13 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bars
 polybar top &
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
+
+
 echo "Bars launched..."
