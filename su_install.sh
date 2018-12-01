2b0c86257b9df972d13d7b2b9ccb54019b7cb6f9
JRozen <thequetran@gmail.com>
2018-12-01 22:51:05 +0100

    add lightdm theme

--- su_install.sh
+++ su_install.sh
@@ -2,3 +2,5 @@
 cp -v ./usr/bin/blurlock        /usr/bin/blurlock
 cp -v ./usr/bin/i3exit          /usr/bin/i3exit
 cp -v ./usr/local/bin/xflock4	/usr/local/bin/xflock4
+cp -v etc/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
+
--- update.sh
+++ update.sh
@@ -21,8 +21,9 @@ cp -vr 	~/.config/dunst 		    ./home/.config/
 cp -vr 	~/.config/termite 		    ./home/.config/
 
 
-cp -v /usr/bin/i3exit               ./usr/bin/i3exit
-cp -v /usr/local/bin/xflock4        ./usr/local/bin/xflock4
-cp -v /usr/bin/blurlock             ./usr/bin/blurlock
+cp -v /usr/bin/i3exit                               ./usr/bin/i3exit
+cp -v /usr/local/bin/xflock4                        ./usr/local/bin/xflock4
+cp -v /usr/bin/blurlock                             ./usr/bin/blurlock
+cp -v /etc/lightdm/lightdm-webkit2-greeter.conf     ./etc/lightdm/lightdm-webkit2-greeter.conf
 
 echo "Done!"
