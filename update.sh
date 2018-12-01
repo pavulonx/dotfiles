695a805ed26e3a657f13851f9827cfec8f2146c7
JRozen <thequetran@gmail.com>
2018-12-01 13:33:39 +0100

    update update scrit

--- update.sh
+++ update.sh
@@ -7,7 +7,6 @@ cp -v ~/.i3/config ./.i3/config
 cp -v ~/.config/polybar/config ./polybar/config
 cp -v ~/.config/polybar/launch.sh ./polybar/launch.sh
 cp -vr ~/.config/polybar ./
-cp -v /etc/i3status.conf ./.i3/i3status.conf
 cp -v ~/.config/i3-scrot.conf ./i3-scrot.conf
 cp -v /usr/bin/blurlock ./blurlock
 
