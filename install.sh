e64b0f83770c7847e5408b4f3b4565271b79f6ff
JRozen <thequetran@gmail.com>
2018-12-01 17:29:58 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-cp -ivr  ./home/* 	~/
-cp -ivr  ./home/.* 	~/
-
+cp -iv    home/.zshrc                        ~/.zshrc
+cp -iv    home/.xbindkeysrc                  ~/.xbindkeysrc
+cp -iv    home/.Xresources                   ~/.Xresources
+cp -iv    home/.xinitrc                      ~/.xinitrc
+cp -iv    home/.vimrc                        ~/.vimrc
+cp -iv    home/.rofi.sh                      ~/.rofi.sh
+cp -iv    home/.aliases.sh                   ~/.aliases.sh
+cp -iv    home/.aliases.sh                   ~/.aliases.sh
+cp -ivr   home/.bin                          ~/.bin              
+cp -ivr   home/.i3                           ~/.i3
+       
+cp -ivr   home/.config                       ~/
