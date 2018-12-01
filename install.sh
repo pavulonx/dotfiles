d9c7d8e83040f22682274715830f0e1deac84c71
JRozen <thequetran@gmail.com>
2018-12-01 17:06:32 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp -v  ./home/.rofi.sh              ~/.rofi.sh
-cp -vr ./home/.config/polybar       ~/.config/
-cp -v ~/.xbindkeysrc                ~/
+cp -ivr  ./home/* 	~/
+cp -ivr  ./home/.* 	~/
+
