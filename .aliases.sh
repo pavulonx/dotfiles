37168657b7d6045faeb10f04035911d70dff1b35
JRozen <thequetran@gmail.com>
2018-12-01 14:55:30 +0100

    update aliases.sh

--- .aliases.sh
+++ .aliases.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+# cd
 alias cdpwr="cd $PWR_HOME"
 alias cdapp="cd ${APPS}"
+
 alias lt='ls -lat'
-alias diff="diff --color"
+
+alias manpacman="cat /usr/share/oh-my-zsh/plugins/archlinux/README.md"
+
+# docker
 alias dr=docker
 alias drps="docker ps"
 alias dre="docker exec"
 alias dreit="docker exec -it"
 alias drk="docker kill"
 alias drc=docker-compose
-alias manpacman="cat /usr/share/oh-my-zsh/plugins/archlinux/README.md"
+
+# git
+alias gitunmodlastcommit='git ls-files --full-name | grep -v "$(git diff --name-only HEAD)"'
