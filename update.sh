10f9f945f9407d5914bd815422ae1c631cc8bc75
JRozen <thequetran@gmail.com>
2018-12-01 15:55:24 +0100

    moved dots

--- update.sh
+++ update.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cp -v ~/.xbindkeysrc .home/
+cp -v 	~/.zshrc 	./home/
+cp -v 	~/.xbindkeysrc 	./home/
+cp -vr 	~/.bin 		./home/
+cp -v 	~/.xinitrc 	./home/
+cp -v 	~/.vimrc 	./home/
+cp -v 	~/.rofi.sh 	./home/
+cp -v 	~/.Xmodmap 	./home/	
+cp -v 	~/.aliases.sh 	./home/
+cat ~/.exports.sh | cut -d= -f1 > ./home/.exports.sh
 
 
 cp -v ~/.config/dunst/dunstrc ./dunst/dunstrc
@@ -10,24 +18,10 @@ cp -v ~/.config/mimeapps.list ./mimeapps.list
 cp -v ~/.config/termite/config ./termite/config
 cp -vr ~/.config/polybar ./home/.config/
 
-cp -v ~/.i3/config ./.i3/config
-
-cp -v ~/.bin/* ./scripts
-cp -v ~/.zshrc ./.zshrc
-
-cp -v ~/.xinitrc ./.xinitrc
-cp -v ~/.extend.xinitrc ./.extend.xinitrc
 cp -v ~/.Xresources ./.Xresources
 cp -v ~/.extend.Xresources ./.extend.Xresources
 
-cp -v ~/.Xmodmap ./.Xmodmap
-cp -v ~/.rofi.sh ./home/.rofi.sh
-
-cp -v ~/.aliases.sh ./.aliases.sh
-cat ~/.exports.sh | cut -d= -f1 > .exports.sh
-
-cp -vr ~/.bin .
-cp -v ~/.vimrc ./.vimrc
+cp -v ~/.i3/config ./.i3/config
 
 cp -v /usr/bin/i3exit          ./usr/bin/i3exit
 cp -v /usr/local/bin/xflock4 ./usr/local/bin/xflock4
